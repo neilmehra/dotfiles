f93bda049270a06b2d937b977ad88521495fcb1e
neil mehra <neilmehra@outlook.com>
2025-02-12 09:45:40 -0500

    fzf jump to ranger

--- ranger/commands.py
+++ ranger/commands.py
@@ -137,3 +137,25 @@ class osuhelper(Command):
 
 
 
+# fzf_locate
+class fzf_locate(Command):
+    """
+    :fzf_locate
+
+    Find a file using fzf.
+
+    With a prefix argument select only directories.
+
+    See: https://github.com/junegunn/fzf
+    """
+    def execute(self):
+        import subprocess
+        command="fd | fzf -e -i"
+        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
